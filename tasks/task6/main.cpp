e07c209ad9679a4598fad21469a1ff8006ff0719
RomanRakov <144253949+RomanRakov@users.noreply.github.com>
2025-05-23 19:33:19 +0000

    RakovHWTask6

--- tasks/task6/main.cpp
+++ tasks/task6/main.cpp
@@ -1,21 +1,3 @@
-/**
- * @file main.cpp
- * @brief задача написать программу, которая будет высчитывать значение интеграла через метод трапеций
- * Особенность этой задачи: использовать потоки(threads) для ускорения вычислений (нужно разделить задачу для вычисления части интеграла на каждый поток)
- *
- * на вход программе при запуске(это в argv[]) подается 3 числа: a, b, n, tn где [a, b] - интервал (целочисленный, неотрицательный и не больше 50 для задачи), n - количество разбиений, tn - threads number - количество потоков для подсчёта
- * примечание: n - количество разбиений в тестах будет явно нацело делиться на tn - количество потоков.
- *
- * на выход программе выведите значение интеграла через метод трапеций (вывод в стандартный поток std::cout, в конце выведите '\n'), вывод в поток с точностью до 4 знака
- * в функции trapezoidalIntegral есть переменная const std::function<double (double)> &f, при вызове подсчётов передайте ссылку на функцию из задания
- * реализовать подсчёт интеграла S(a, b) = (1+e^x)^0.5 dx
- * 
- *
- * литература:
- * https://ru.wikipedia.org/wiki/Метод_трапеций
- * https://habr.com/ru/articles/420867/
- */
-
 #include <cmath>
 #include <functional>
 #include <numeric>
@@ -24,7 +6,7 @@
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
-
+#include <string>
 
 class Integral {
 private:
@@ -32,35 +14,55 @@ private:
 
 public:
     Integral(int& argc, char** argv) {
-        if (argc != 5) {
-            throw std::invalid_argument("Wrong number of arguments");
-        }
-        a  = std::stoi(argv[1]);
-        b  = std::stoi(argv[2]);
-        n  = std::stoi(argv[3]);
+        a = std::stoi(argv[1]);
+        b = std::stoi(argv[2]);
+        n = std::stoi(argv[3]);
         tn = std::stoi(argv[4]);
     }
 
-
     static double integralFunction(double x) {
-        // тут нужно реализовать функцию интеграла S(a, b) = (1+e^x)^0.5 dx
-        return 0;
+        return std::sqrt(1 + std::exp(x));
     }
 
+    double trapezoidalIntegral(double start, double end, int steps, const std::function<double(double)>& f) {
+        double h = (end - start) / steps;
+        double sum = 0.5 * (f(start) + f(end));
+        for (int i = 1; i < steps; ++i) {
+            sum += f(start + i * h);
+        }
+        return sum * h;
+    }
 
     double calculateIntegral() {
-        // в зависимости от количество потоков (tn) реализуйте подсчёт интеграла
-        return 0;
+        std::vector<std::thread> threads;
+        std::vector<double> partialResults(tn, 0.0);
+
+        double h = static_cast<double>(b - a) / n;
+        int stepsPerThread = n / tn;
+
+        auto f = integralFunction;
+
+        for (int i = 0; i < tn; ++i) {
+            double start = a + i * stepsPerThread * h;
+            double end = start + stepsPerThread * h;
+
+            threads.emplace_back([&, i, start, end]() {
+                partialResults[i] = trapezoidalIntegral(start, end, stepsPerThread, f);
+                });
+        }
+
+        for (auto& t : threads) {
+            t.join();
+        }
+
+        return std::accumulate(partialResults.begin(), partialResults.end(), 0.0);
     }
-
 };
-
-
 
 int main(int argc, char** argv)
 {
     auto i = Integral(argc, argv);
-    std::cout << std::fixed << std::setprecision (4);
-    std::cout << i.calculateIntegral() << std::endl;
+    std::cout << std::fixed << std::setprecision(4);
+    std::cout << i.calculateIntegral() << '\n';
     return 0;
 }
